008be2926f1982092069934fa5d5b7abb942030e
Swice <swice@SwirtualBox.(none)>
2013-11-17 03:20:12 -0500

    ready to do maps

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,4 +4,19 @@ class PlacesController < ApplicationController
 		@place = Place.last
 	end
 
+	def new
+		# @place = Place.last
+		@place = Place.new
+	end
+
+	def create
+		@place = Place.create( place_params)
+		redirect_to root_path
+	end
+
+	private
+	def place_params
+		params.require(:place).permit(:title, :address)
+	end
+
 end
